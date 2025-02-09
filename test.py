dfc12bb7df443324271e5ca423d35a6f762c419e
unknown <saculojosh1600@gmail.com>
2025-02-10 02:14:42 +0800

    commit

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-cars = ["lambo","green","large"]
-print(cars)
-print("HELLO WORLD")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
