79b02ac354ee7f725c7b3d584c7962ed40bb044a
unknown <saculojosh1600@gmail.com>
2025-02-17 01:06:29 +0800

    I'm adding javascript animation typing for my name

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,21 @@
-function myFunction() 
-{
- 
-  document.getElementById("myCheck").style.cssText = "background-color: red; color: white; font-size: 44px";
+document.addEventListener("DOMContentLoaded", function() {
+  const name = "Joshua Saculo";
+  let index = 0;
+  let typingElement = document.getElementById("typing");
 
+  function typeEffect() {
+      if (index < name.length) {
+          typingElement.innerHTML += name.charAt(index);
+          index++;
+          setTimeout(typeEffect, 150);
+      } else {
+          setTimeout(() => {
+              typingElement.innerHTML = "";
+              index = 0;
+              typeEffect();
+          }, 2000);
+      }
+  }
 
-}
+  typeEffect();
+});
