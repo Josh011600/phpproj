b373b389f3cd978125e7aec576060e036118149f
unknown <saculojosh1600@gmail.com>
2025-03-07 02:39:07 +0800

    add all

---
+++ static/js/script2.js
@@ -0,0 +1,13 @@
+
+const numbers = ["45", "4", "9", "16", "25"];
+
+let txt = "";
+myFunction();
+document.getElementById("demo").innerHTML = txt;
+
+function myFunction() {
+  for(let i=0;i<=3;i++)
+  {
+    txt +=document.getElementById("demo").innerHTML = numbers[i];
+  }
+}
