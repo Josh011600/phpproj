696f33647464fc962980407ef508ef48fc1b3c20
unknown <saculojosh1600@gmail.com>
2025-03-17 17:44:58 +0800

    add all test

--- static/js/script2.js
+++ static/js/script2.js
@@ -1,13 +1,4 @@
 
 const numbers = ["45", "4", "9", "16", "25"];
 
-let txt = "";
-myFunction();
-document.getElementById("demo").innerHTML = txt;
 
-function myFunction() {
-  for(let i=0;i<=3;i++)
-  {
-    txt +=document.getElementById("demo").innerHTML = numbers[i] + '<br>';
-  }
-}
