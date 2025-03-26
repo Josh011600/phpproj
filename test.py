c5a6bc4b0be1e79fbcfa6376d77477b3da2bed12
unknown <saculojosh1600@gmail.com>
2025-03-26 17:21:18 +0800

    test db

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-
+import sqlite3
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('test.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Create a SQL connection to our SQLite database
+con = sqlite3.connect("data/db1.db")
+
+cur = con.cursor()
+
+# The result of a "cursor.execute" can be iterated over by row
+for row in cur.execute('SELECT * FROM users;'):
+    print(row)
+
+# Be sure to close the connection
+con.close()
+
+
