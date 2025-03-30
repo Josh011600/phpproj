52e1aef8fbddc2ff826056639cbf4678c5a2ccef
unknown <saculojosh1600@gmail.com>
2025-03-31 04:10:23 +0800

    Database Connection: Debugging

--- test_app.py
+++ test_app.py
@@ -1,4 +1,19 @@
-import pytest
+from flask import Flask, render_template
 
-def test_hello_world():
-    assert 1 + 1 == 2
+import sqlite3
+app=Flask(__name__)
+app.secret_key='__privatekey__'
+
+#Default home page
+def defaultHome():
+    return render_template('test.html')
+
+def __init__(self):
+    con=sqlite3.connect('db1.db')
+    c=con.cursor
+    c.execute("create table user1(name text, password text)")
+    con.commit()
+
+
+if __name__ == "__main__":
+    app.run()  
