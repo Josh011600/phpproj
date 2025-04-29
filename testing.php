e549669e99803159395c6cbf2bc136fe9a59a141
Josh011600 <114022933+Josh011600@users.noreply.github.com>
2025-04-30 00:04:26 +0800

    Learning-phase: removed some files

---
+++ testing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $books = ["book1", "book2"];
+        $books43 = ["alamat ng hotdog", "alamat ng ampalaya"];
+        ?>
+    <ul> 
+       <?php foreach ($books43 as $book45) : ?>
+            <li><?php echo $book45['1']; ?></li>
+            
+       <?php endforeach; ?>
+
+       </ul> 
+
+   
+</body>
+</html>
